2ae6b9e02cb36751c24e14ed206c955d9a26cb6d
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-18 09:27:30 +0100

    Enviamos mensajes desde la app

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -10,15 +10,17 @@ export class ChatComponent {
 
   mensaje: string = "";
 
-  constructor(public _chatService:ChatService) {
-      this._chatService.cargarMensajes().subscribe(
-         ( mensajes:any[])=>{
-              console.log(mensajes)
-          }
-      )
+  constructor(public _cs:ChatService) {
+      this._cs.cargarMensajes().subscribe()
   }
 
   enviar_mensaje(){
       console.log(this.mensaje)
+
+      if(this.mensaje.length>0){
+          this._cs.agregarMensaje(this.mensaje)
+            .then(()=>this.mensaje="")
+            .catch((err)=>console.error('Error al enviar',err))
+      }
   }
 }
--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Mensaje } from '../interfaces/mensaje.interface';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-    private itemsCollection: AngularFirestoreCollection<any>;
-    items: Observable<any[]>;
+    private itemsCollection: AngularFirestoreCollection<Mensaje>;
+    items: Observable<Mensaje[]>;
 
-    public chat: any[] = [];
+    public chats: Mensaje[] = [];
 
     constructor(private afs: AngularFirestore) {
 
     }
-    addItem(item: any) {
+    addItem(item: Mensaje) {
        this.itemsCollection.add(item);
     }
 
     cargarMensajes() {
-        this.itemsCollection = this.afs.collection<any>('Chats');
-        return this.itemsCollection.valueChanges();
+        this.itemsCollection = this.afs.collection<Mensaje>('Chats');
+        return this.itemsCollection
+                .valueChanges()
+                .pipe(
+                    map( (mensajes: Mensaje[]) =>{
+                        console.log( mensajes );
+                        this.chats =  mensajes;
+                    } )
+                );
+    }
+
+    agregarMensaje( texto:string ){
+        // TODO: uid
+        let mensaje: Mensaje = {
+            nombre: '',
+            mensaje: texto,
+            fecha: new Date().getTime()
+        }
+
+        return this.itemsCollection.add(mensaje);
     }
 }
